71c4d806a4fa607645af315785c18bc766271518
aram <aram.ds@gmail.com>
2020-09-19 13:29:31 +0400

    Add: translation

--- src/Module.php
+++ src/Module.php
@@ -3,14 +3,17 @@ declare(strict_types=1);
 
 namespace devnullius\user;
 
+use Yii;
 use yii\base\Module as BaseModule;
+use yii\i18n\PhpMessageSource;
 
 class Module extends BaseModule
 {
+    public const VERSION = '1.0.0';
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'core\modules\user\controllers';
+    public $controllerNamespace = 'devnullius\user\controllers';
 
     /**
      * {@inheritdoc}
@@ -19,6 +22,35 @@ class Module extends BaseModule
     {
         parent::init();
 
+        $this->registerTranslations();
         // custom initialization code goes here
+    }
+
+    /**
+     * Register translations for this module
+     */
+    final public function registerTranslations(): void
+    {
+        if (!isset(Yii::$app->get('i18n')->translations['modules/user/*'])) {
+            Yii::$app->get('i18n')->translations['modules/user/*'] = [
+                'class' => PhpMessageSource::class,
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+
+    /**
+     * Translate module message
+     *
+     * @param string      $category
+     * @param string      $message
+     * @param array       $params
+     * @param string|null $language
+     *
+     * @return string
+     */
+    public static function t(string $category, string $message, array $params = [], string $language = null): string
+    {
+        return Yii::t('modules/user/' . $category, $message, $params, $language);
     }
 }
